b3136fcbaa8bb1ec0933d705abd5f586a56fdc49
Radek Sienkiewicz <mail@velvetshark.com>
2024-08-10 20:36:43 +0200

    Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,7 @@ export default async function Index() {
       <Navigation proposals={proposals || []} />
       <main className="flex-1 h-full overflow-y-auto">
         <div className="flex-1 px-6 pb-10 items-center justify-center md:justify-start text-center gap-y-8">
-          <h1 className="text-4xl font-bold">Proposals</h1>
-          {proposals && proposals.length > 0 ? (
-            <div>
-              {proposals.map((proposal) => (
-                <div key={proposal.id}>
-                  <h3 className="font-semibold text-lg">
-                    {proposal.proposal_type}-{proposal.number} {proposal.title}
-                  </h3>
-                  <p>{proposal.description}</p>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <p>No proposals found.</p>
-          )}
+          <h1 className="text-4xl font-bold">Homepage</h1>
         </div>
       </main>
     </div>
